e8118ce51ae278a9636226284fb3ac5458388fac
dtvabrand <marcobertolone@outlook.com>
2025-12-11 11:10:53 +0100

    Update trakt_lists.py

--- trakt_lists.py
+++ trakt_lists.py
@@ -45,7 +45,7 @@ SITES=[
 ]
 
 QIDS = [
-    ("The Mother, the Menacer, and Me", ""),
+    ("The Mother, the Menacer, and Me", "Q137352527"),
     ("A Breed Apart", "Q133845943"),
     ("A Spartan Dream", "Q136655065"),
     ("Aftershock: The Nicole P Bell Story", "Q137103837"),
