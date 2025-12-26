00846780ba5fceff0ac4833101cd1e17ca66be22
dtvabrand <mbncaaf@gmail.com>
2025-12-26 12:03:24 +0100

    Update trakt_lists.py

--- trakt_lists.py
+++ trakt_lists.py
@@ -1784,7 +1784,7 @@ def main():
     with ThreadPoolExecutor(max_workers=WD_WORKERS) as ex:
         futs=[ex.submit(S.get,"https://www.wikidata.org/w/api.php",
                         params={"action":"wbgetentities","format":"json","formatversion":2,
-                                "ids":"|".join(qids[i:i+50]),"props":"claims","redirects":1},
+                                "ids":"|".join(qids[i:i+50]),"props":"claims"},
                         timeout=HTTP_TIMEOUT) for i in range(0,len(qids),50)]
         for fu in as_completed(futs):
             try:
